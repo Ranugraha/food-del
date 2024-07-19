114d761e0e8ed8cc9fe4b95432f26a2220d329f7
Ridwan Anugrah <awan72.ra@gmail.com>
2024-07-19 17:25:29 +0700

    add footer done

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/exploreMenu/ExploreMenu';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 import Footer from '../../components/Footer/Footer';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
   const [category, setCategory] = useState('ALL');
@@ -14,6 +15,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
