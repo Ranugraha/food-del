acf665d4ad1a94f87c36f1c0b01ca6d6ff6b8f8e
Ridwan Anugrah <awan72.ra@gmail.com>
2024-07-19 20:24:39 +0700

    fixing responsif

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/placeOrder';
 import Footer from './components/Footer/Footer';
+import LoginPopUp from './components/loginPopUp/LoginPopUp';
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+      {showLogin ? <LoginPopUp /> : <></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={showLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
---
+++ frontend/src/components/loginPopUp/LoginPopUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './loginPopUp.css';
+
+const LoginPopUp = () => {
+  return (
+    <div div className="login-pop-up">
+      LoginPopUp
+    </div>
+  );
+};
+
+export default LoginPopUp;
